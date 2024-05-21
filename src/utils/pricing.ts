9163deb7896dbe6b54a1e252c1ff522acf1f5b55
Nouman Mahmood <noumanmhd@outlook.com>
2024-05-21 10:58:54 +0300

    Forked from @Uniswap v3-subgraph

---
+++ src/utils/pricing.ts
@@ -0,0 +1,136 @@
+/* eslint-disable prefer-const */
+import { ONE_BD, ZERO_BD, ZERO_BI } from './constants'
+import { Bundle, Pool, Token } from './../types/schema'
+import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
+import { exponentToBigDecimal, safeDiv } from '../utils/index'
+
+const WETH_ADDRESS = '0x56936a13db61860f344f26adeb572d91b21c65b2'
+const USDC_WETH_03_POOL = '0x700a9c524860f2508119e8ad025ea2275071d6a3'
+
+// token where amounts should contribute to tracked volume and liquidity
+// usually tokens that many tokens are paired with s
+export let WHITELIST_TOKENS: string[] = [
+  WETH_ADDRESS, // WETH
+  '0xc5be71205ad0023923c367b3e9638cbe9967691f',
+]
+
+let STABLE_COINS: string[] = [
+  '0xc5be71205ad0023923c367b3e9638cbe9967691f',
+]
+let MINIMUM_ETH_LOCKED = BigDecimal.fromString('0')
+
+let Q192 = BigInt.fromI32(2).pow(192 as u8)
+
+export function sqrtPriceX96ToTokenPrices(sqrtPriceX96: BigInt, token0: Token, token1: Token): BigDecimal[] {
+  let num = sqrtPriceX96.times(sqrtPriceX96).toBigDecimal()
+  let denom = BigDecimal.fromString(Q192.toString())
+  let price1 = num
+    .div(denom)
+    .times(exponentToBigDecimal(token0.decimals))
+    .div(exponentToBigDecimal(token1.decimals))
+  let price0 = safeDiv(BigDecimal.fromString('1'), price1)
+  return [price0, price1]
+}
+
+export function getEthPriceInUSD(): BigDecimal {
+  let usdcPool = Pool.load(USDC_WETH_03_POOL)
+  if (usdcPool !== null) {
+    // Fixing the Token
+    return usdcPool.token1Price
+  } else {
+    return ZERO_BD
+  }
+}
+
+/**
+ * Search through graph to find derived Eth per token.
+ * @todo update to be derived ETH (add stablecoin estimates)
+ **/
+export function findEthPerToken(token: Token): BigDecimal {
+  if (token.id == WETH_ADDRESS) {
+    return ONE_BD
+  }
+  let whiteList = token.whitelistPools
+  // for now just take USD from pool with greatest TVL
+  // need to update this to actually detect best rate based on liquidity distribution
+  let largestLiquidityETH = ZERO_BD
+  let priceSoFar = ZERO_BD
+  let bundle = Bundle.load('1')!
+
+  // hardcoded fix for incorrect rates
+  // if whitelist includes token - get the safe price
+  if (STABLE_COINS.includes(token.id)) {
+    priceSoFar = safeDiv(ONE_BD, bundle.ethPriceUSD)
+  } else {
+    for (let i = 0; i < whiteList.length; ++i) {
+      let poolAddress = whiteList[i]
+      let pool = Pool.load(poolAddress)
+
+      if (pool) {
+        if (pool.liquidity.gt(ZERO_BI)) {
+          if (pool.token0 == token.id) {
+            // whitelist token is token1
+            let token1 = Token.load(pool.token1)
+            // get the derived ETH in pool
+            if (token1) {
+              let ethLocked = pool.totalValueLockedToken1.times(token1.derivedETH)
+              if (ethLocked.gt(largestLiquidityETH) && ethLocked.gt(MINIMUM_ETH_LOCKED)) {
+                largestLiquidityETH = ethLocked
+                // token1 per our token * Eth per token1
+                priceSoFar = pool.token1Price.times(token1.derivedETH as BigDecimal)
+              }
+            }
+          }
+          if (pool.token1 == token.id) {
+            let token0 = Token.load(pool.token0)
+            // get the derived ETH in pool
+            if (token0) {
+              let ethLocked = pool.totalValueLockedToken0.times(token0.derivedETH)
+              if (ethLocked.gt(largestLiquidityETH) && ethLocked.gt(MINIMUM_ETH_LOCKED)) {
+                largestLiquidityETH = ethLocked
+                // token0 per our token * ETH per token0
+                priceSoFar = pool.token0Price.times(token0.derivedETH as BigDecimal)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return priceSoFar // nothing was found return 0
+}
+
+/**
+ * Accepts tokens and amounts, return tracked amount based on token whitelist
+ * If one token on whitelist, return amount in that token converted to USD * 2.
+ * If both are, return sum of two amounts
+ * If neither is, return 0
+ */
+export function getTrackedAmountUSD(
+  tokenAmount0: BigDecimal,
+  token0: Token,
+  tokenAmount1: BigDecimal,
+  token1: Token
+): BigDecimal {
+  let bundle = Bundle.load('1')!
+  let price0USD = token0.derivedETH.times(bundle.ethPriceUSD)
+  let price1USD = token1.derivedETH.times(bundle.ethPriceUSD)
+
+  // both are whitelist tokens, return sum of both amounts
+  if (WHITELIST_TOKENS.includes(token0.id) && WHITELIST_TOKENS.includes(token1.id)) {
+    return tokenAmount0.times(price0USD).plus(tokenAmount1.times(price1USD))
+  }
+
+  // take double value of the whitelisted token amount
+  if (WHITELIST_TOKENS.includes(token0.id) && !WHITELIST_TOKENS.includes(token1.id)) {
+    return tokenAmount0.times(price0USD).times(BigDecimal.fromString('2'))
+  }
+
+  // take double value of the whitelisted token amount
+  if (!WHITELIST_TOKENS.includes(token0.id) && WHITELIST_TOKENS.includes(token1.id)) {
+    return tokenAmount1.times(price1USD).times(BigDecimal.fromString('2'))
+  }
+
+  // neither token is on white list, tracked amount is 0
+  return ZERO_BD
+}
---
+++ src/utils/token.ts
@@ -0,0 +1,93 @@
+/* eslint-disable prefer-const */
+import { ERC20 } from '../types/Factory/ERC20'
+import { ERC20SymbolBytes } from '../types/Factory/ERC20SymbolBytes'
+import { ERC20NameBytes } from '../types/Factory/ERC20NameBytes'
+import { StaticTokenDefinition } from './staticTokenDefinition'
+import { BigInt, Address } from '@graphprotocol/graph-ts'
+import { isNullEthValue } from '.'
+
+export function fetchTokenSymbol(tokenAddress: Address): string {
+  let contract = ERC20.bind(tokenAddress)
+  let contractSymbolBytes = ERC20SymbolBytes.bind(tokenAddress)
+
+  // try types string and bytes32 for symbol
+  let symbolValue = 'unknown'
+  let symbolResult = contract.try_symbol()
+  if (symbolResult.reverted) {
+    let symbolResultBytes = contractSymbolBytes.try_symbol()
+    if (!symbolResultBytes.reverted) {
+      // for broken pairs that have no symbol function exposed
+      if (!isNullEthValue(symbolResultBytes.value.toHexString())) {
+        symbolValue = symbolResultBytes.value.toString()
+      } else {
+        // try with the static definition
+        let staticTokenDefinition = StaticTokenDefinition.fromAddress(tokenAddress)
+        if (staticTokenDefinition != null) {
+          symbolValue = staticTokenDefinition.symbol
+        }
+      }
+    }
+  } else {
+    symbolValue = symbolResult.value
+  }
+
+  return symbolValue
+}
+
+export function fetchTokenName(tokenAddress: Address): string {
+  let contract = ERC20.bind(tokenAddress)
+  let contractNameBytes = ERC20NameBytes.bind(tokenAddress)
+
+  // try types string and bytes32 for name
+  let nameValue = 'unknown'
+  let nameResult = contract.try_name()
+  if (nameResult.reverted) {
+    let nameResultBytes = contractNameBytes.try_name()
+    if (!nameResultBytes.reverted) {
+      // for broken exchanges that have no name function exposed
+      if (!isNullEthValue(nameResultBytes.value.toHexString())) {
+        nameValue = nameResultBytes.value.toString()
+      } else {
+        // try with the static definition
+        let staticTokenDefinition = StaticTokenDefinition.fromAddress(tokenAddress)
+        if (staticTokenDefinition != null) {
+          nameValue = staticTokenDefinition.name
+        }
+      }
+    }
+  } else {
+    nameValue = nameResult.value
+  }
+
+  return nameValue
+}
+
+export function fetchTokenTotalSupply(tokenAddress: Address): BigInt {
+  let contract = ERC20.bind(tokenAddress)
+  let totalSupplyValue = BigInt.zero()
+  let totalSupplyResult = contract.try_totalSupply()
+  if (!totalSupplyResult.reverted) {
+    totalSupplyValue = totalSupplyResult.value
+  }
+  return totalSupplyValue
+}
+
+export function fetchTokenDecimals(tokenAddress: Address): BigInt | null {
+  let contract = ERC20.bind(tokenAddress)
+  // try types uint8 for decimals
+  let decimalResult = contract.try_decimals()
+
+  if (!decimalResult.reverted) {
+    if (decimalResult.value.lt(BigInt.fromI32(255))) {
+      return decimalResult.value
+    }
+  } else {
+    // try with the static definition
+    let staticTokenDefinition = StaticTokenDefinition.fromAddress(tokenAddress)
+    if (staticTokenDefinition) {
+      return staticTokenDefinition.decimals
+    }
+  }
+
+  return null
+}
